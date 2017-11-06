0a7386537456c1f4735674e92fede11bae487519
Austin Bean <austinbean@gmail.com>
2017-11-05 20:55:40 -0600

    still working

--- censprob.jl
+++ censprob.jl
@@ -11,6 +11,8 @@ include("DataGen.jl")
 # x1, x2, x3 ~ Normal(0,1) iid.
 
 inp1 = DataGen2(20); 
+inp1 = sortrows(inp1, by=x->x[1]) # returns sorted by first column.  All options with 1 first.  
+ny = convert(Int64, sum(inp1[:,1]))
 
 data = Dict{Symbol,Any}(
     :y => inp1[:,1],
@@ -25,33 +27,29 @@ data[:xm] = hcat(data[:x1], data[:x2], data[:x3])
 Some notes from examples... 
 - in "bones" an intermediate vector can be created in the main stochastic node to hold some values.  
 - this may require defining my own sampler.  See the tutorial on that, I think.  
+
+Design the sampler.... this should do something like in two blocks
+draw conditional on previous val.  I think this can be done.
+Create an intermediate vector in the sampler, I think.  
+
+
 =#
 
 m1 = Model(
 
     y = Stochastic(1, # this is 1-dimensional since β1, β2, β3 all appear 
-        (mu, sigma, ll, ul) -> [TruncatedNormal(mu,sqrt(sigma) ,ll ,ul) for i =1:length(mu)],
+        (mu, sigma, ll, ul) -> begin # note that zeros come first...
+            if 
+                [TruncatedNormal(mu,sqrt(sigma),ll ,ul) for i =1:ny-1], # see if parameter ny from global scope can be accessed here.
+            else 
+
+            end 
+        end
         false),
-
-#=
-To do: reorganize the matrix inp1.  Maybe sort in advance.
-Design the sampler.... this should do something like in two blocks
-draw conditional on previous val.  I think this can be done.
-Create an intermediate vector in the sampler, I think.  
-=#
-
 
     mu = Logical(1,
         (xm,β) -> xm*β,
         false),
-
-    ll = Logical(
-        () -> 0
-        ),
-
-    ul = Logical(
-        () -> 10
-        ),
 
     β = Stochastic(1,
         () -> MvNormal(3, sqrt(1000)) # draws three independent normal parameters β1 - β3 
